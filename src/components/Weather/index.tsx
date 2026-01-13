33b9f32ce45dddccb3fbcc803e4f818f254241f9
Edward Konovalov <edw4rd.young@gmail.com>
2026-01-13 10:24:13 +0300

    fixes

--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -1,4 +1,5 @@
-import { Flex, Group, Stack, Text } from "@mantine/core";
+import { Flex, Group, Skeleton, Stack, Text } from "@mantine/core";
+import { range } from "lodash-es";
 import { useWeatherData } from "../../api/useWeatherData";
 import { weatherCodes } from "../../api/weatherCodes";
 import { useClock } from "../../hooks/useClock";
@@ -7,162 +8,200 @@ import { degreesToCompass } from "../../utils/degrees-to-compas";
 import { SmartIcon } from "../SmartIcon";
 import { WeatherBadge } from "./weatherBadge";
 
+function WeatherSkeleton() {
+	return (
+		<>
+			<Group>
+				<Skeleton
+					height={28}
+					width={186}
+					radius="md"
+				/>
+			</Group>
+			<Group>
+				{range(7).map((i) => (
+					<Skeleton
+						height={24}
+						width={140}
+						radius="xl"
+						key={i}
+					/>
+				))}
+			</Group>
+		</>
+	);
+}
+
+function DateTime() {
+	const { date, time } = useClock();
+	return (
+		<Stack gap={"xxs"}>
+			<Text
+				component={"a"}
+				href={"https://calendar.google.com/calendar/u/1/r"}
+			>
+				<Group
+					gap={"xxs"}
+					align={"center"}
+				>
+					{date}
+					<SmartIcon icon={"mdi:launch"} />
+				</Group>
+			</Text>
+			<Text
+				lh={"var(--mantine-line-height-xxs)"}
+				size={"xxl"}
+				w={"165px"}
+			>
+				{time}
+			</Text>
+		</Stack>
+	);
+}
+
 export function WeatherWidget() {
 	const { data, isPending, error } = useWeatherData({
 		latitude: weatherLocation.lat,
 		longitude: weatherLocation.lon,
 	});
-	const { date, time } = useClock();
-
-	if (!data || isPending || error) {
-		return null;
-	}
 
 	return (
 		<Stack gap={"sm"}>
-			<Stack gap={"xxs"}>
-				<Text
-					component={"a"}
-					href={"https://calendar.google.com/calendar/u/1/r"}
-				>
-					<Group
-						gap={"xxs"}
-						align={"center"}
-					>
-						{date}
-						<SmartIcon icon={"mdi:launch"} />
+			<DateTime />
+			{!data || isPending || error ? (
+				<WeatherSkeleton />
+			) : (
+				<>
+					<Group>
+						<Text size={"xl"}>{data?.current.temperature_2m}&deg;</Text>
+						<Group gap={"0"}>
+							<img
+								src={
+									weatherCodes[
+										data?.current
+											?.weather_code as unknown as keyof typeof weatherCodes
+									][data?.current.is_day ? "day" : "night"].image
+								}
+								alt={
+									weatherCodes[
+										data?.current
+											?.weather_code as unknown as keyof typeof weatherCodes
+									][data?.current.is_day ? "day" : "night"].description
+								}
+								width={32}
+							/>
+							Feels like {data?.current.apparent_temperature}&deg;
+						</Group>
 					</Group>
-				</Text>
-				<Text
-					lh={"var(--mantine-line-height-xxs)"}
-					size={"xxl"}
-					w={"165px"}
-				>
-					{time}
-				</Text>
-			</Stack>
-			<Group>
-				<Text size={"xl"}>{data?.current.temperature_2m}&deg;</Text>
-				<Group gap={"0"}>
-					<img
-						src={
-							weatherCodes[
-								data?.current
-									?.weather_code as unknown as keyof typeof weatherCodes
-							][data?.current.is_day ? "day" : "night"].image
-						}
-						alt={
-							weatherCodes[
-								data?.current
-									?.weather_code as unknown as keyof typeof weatherCodes
-							][data?.current.is_day ? "day" : "night"].description
-						}
-						width={32}
-					/>
-					Feels like {data?.current.apparent_temperature}&deg;
-				</Group>
-			</Group>
-			<Flex
-				component="a"
-				gap="xs"
-				align="center"
-				wrap={"wrap"}
-				href={`https://yandex.ru/pogoda/ru?lat=${weatherLocation.lat}&lon=${weatherLocation.lon}`}
-				target="_blank"
-				rel="noreferrer"
-				style={{ textDecoration: "none", color: "inherit" }}
-			>
-				<WeatherBadge
-					icon={{
-						icon: "mdi:drop",
-						color: "var(--mantine-color-blue-5)",
-					}}
-				>
-					{data?.current.relative_humidity_2m}% (dew at{" "}
-					{data?.current.dew_point_2m}&deg;)
-				</WeatherBadge>
-				<WeatherBadge
-					icon={{ icon: "tabler:wind", color: "var(--mantine-color-blue-5)" }}
-				>
-					{data?.current.wind_speed_10m} m/s (up to{" "}
-					{data?.current.wind_gusts_10m} m/s){" "}
-					<SmartIcon icon={"mdi:compass-outline"} />
-					{degreesToCompass(data?.current.wind_direction_10m)}{" "}
-				</WeatherBadge>
-				<WeatherBadge
-					icon={{
-						icon: "solar:sun-bold",
-						color: "var(--mantine-color-yellow-8)",
-					}}
-				>
-					{[data?.current.uv_index_max, data?.current.uv_index_clear_sky_max]
-						.sort()
-						.join("-")}{" "}
-					UV
-				</WeatherBadge>
-				<WeatherBadge icon={"lets-icons:pressure"}>
-					<span>{data?.current.pressure_msl}</span> hPa
-				</WeatherBadge>
-				{data?.current.precipitation ||
-				data?.current.precipitation_probability ? (
-					<WeatherBadge icon={"material-symbols:weather-mix-rounded"}>
-						{data?.current.precipitation &&
+					<Flex
+						component="a"
+						gap="xs"
+						align="center"
+						wrap={"wrap"}
+						href={`https://yandex.ru/pogoda/ru?lat=${weatherLocation.lat}&lon=${weatherLocation.lon}`}
+						target="_blank"
+						rel="noreferrer"
+						style={{ textDecoration: "none", color: "inherit" }}
+					>
+						<WeatherBadge
+							icon={{
+								icon: "mdi:drop",
+								color: "var(--mantine-color-blue-5)",
+							}}
+						>
+							{data?.current.relative_humidity_2m}% (dew at{" "}
+							{data?.current.dew_point_2m}&deg;)
+						</WeatherBadge>
+						<WeatherBadge
+							icon={{
+								icon: "tabler:wind",
+								color: "var(--mantine-color-blue-5)",
+							}}
+						>
+							{data?.current.wind_speed_10m} m/s (up to{" "}
+							{data?.current.wind_gusts_10m} m/s){" "}
+							<SmartIcon icon={"mdi:compass-outline"} />
+							{degreesToCompass(data?.current.wind_direction_10m)}{" "}
+						</WeatherBadge>
+						<WeatherBadge
+							icon={{
+								icon: "solar:sun-bold",
+								color: "var(--mantine-color-yellow-8)",
+							}}
+						>
+							{[
+								data?.current.uv_index_max,
+								data?.current.uv_index_clear_sky_max,
+							]
+								.sort()
+								.join("-")}{" "}
+							UV
+						</WeatherBadge>
+						<WeatherBadge icon={"lets-icons:pressure"}>
+							<span>{data?.current.pressure_msl}</span> hPa
+						</WeatherBadge>
+						{data?.current.precipitation ||
 						data?.current.precipitation_probability ? (
-							<>
-								<span>{data?.current.precipitation} mm/h</span> (
-								{data?.current.precipitation_probability}% probability)
-							</>
-						) : data?.current.precipitation ? (
-							<span>{data?.current.precipitation}mm/h</span>
-						) : (
-							<>{data?.current.precipitation_probability}% probability</>
-						)}
-					</WeatherBadge>
-				) : null}
-				{data?.current.rain ||
-				data?.current.snowfall ||
-				data?.current.snow_depth ? (
-					<WeatherBadge>
-						{data?.current.rain ? (
-							<>
-								<SmartIcon
-									icon={{
-										icon: "material-symbols:rainy-light",
-										color: "var(--mantine-color-blue-5)",
-									}}
-								/>
-								<span>{data?.current.rain} mm/h </span>
-							</>
+							<WeatherBadge icon={"material-symbols:weather-mix-rounded"}>
+								{data?.current.precipitation &&
+								data?.current.precipitation_probability ? (
+									<>
+										<span>{data?.current.precipitation} mm/h</span> (
+										{data?.current.precipitation_probability}% probability)
+									</>
+								) : data?.current.precipitation ? (
+									<span>{data?.current.precipitation}mm/h</span>
+								) : (
+									<>{data?.current.precipitation_probability}% probability</>
+								)}
+							</WeatherBadge>
 						) : null}
-						{data?.current.snowfall ? (
-							<>
-								<SmartIcon
-									icon={{
-										icon: "mdi:snowflake",
-										color: "var(--mantine-color-blue-5)",
-									}}
-								/>
-								<span>{data?.current.snowfall} cm/h </span>
-							</>
+						{data?.current.rain ||
+						data?.current.snowfall ||
+						data?.current.snow_depth ? (
+							<WeatherBadge>
+								{data?.current.rain ? (
+									<>
+										<SmartIcon
+											icon={{
+												icon: "material-symbols:rainy-light",
+												color: "var(--mantine-color-blue-5)",
+											}}
+										/>
+										<span>{data?.current.rain} mm/h </span>
+									</>
+								) : null}
+								{data?.current.snowfall ? (
+									<>
+										<SmartIcon
+											icon={{
+												icon: "mdi:snowflake",
+												color: "var(--mantine-color-blue-5)",
+											}}
+										/>
+										<span>{data?.current.snowfall} cm/h </span>
+									</>
+								) : null}
+								{data?.current.snow_depth ? (
+									<>
+										<SmartIcon
+											icon={{
+												icon: "icon-park-outline:row-height",
+												color: "var(--mantine-color-blue-5)",
+											}}
+										/>
+										<span>{data?.current.snow_depth * 100} cm</span>
+									</>
+								) : null}
+							</WeatherBadge>
 						) : null}
-						{data?.current.snow_depth ? (
-							<>
-								<SmartIcon
-									icon={{
-										icon: "icon-park-outline:row-height",
-										color: "var(--mantine-color-blue-5)",
-									}}
-								/>
-								<span>{data?.current.snow_depth * 100} cm</span>
-							</>
-						) : null}
-					</WeatherBadge>
-				) : null}
-				<WeatherBadge icon={"solar:eye-linear"}>
-					{data?.current.visibility / 1000} km
-				</WeatherBadge>
-				<SmartIcon icon={"mdi:launch"} />
-			</Flex>
+						<WeatherBadge icon={"solar:eye-linear"}>
+							{data?.current.visibility / 1000} km
+						</WeatherBadge>
+						<SmartIcon icon={"mdi:launch"} />
+					</Flex>
+				</>
+			)}
 		</Stack>
 	);
 }
--- src/index.tsx
+++ src/index.tsx
@@ -22,6 +22,7 @@ export function WidgetsGrid(props: { config: WidgetsConfig }) {
 		<Grid
 			grow
 			gutter={"xs"}
+			w={"100%"}
 		>
 			{props.config.widgets.map((w, i) => (
 				<GridCol
